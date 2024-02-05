d8ed1434e3e2211c5d6be66336d33fd028452503
hrithikrj06 <126853369+hrithikrj06@users.noreply.github.com>
2024-02-05 22:59:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -86,9 +86,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = ""
+        URL = "https://academic-harriott-filmyinsaan06.koyeb.app/"
     else:
-        URL = ""
+        URL = "https://academic-harriott-filmyinsaan06.koyeb.app/"
 
 
 
